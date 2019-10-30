7f0dca5d21a9de996e24fb7e0770050fdd8dd730
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-30 13:48:10 +0600

    third commit

--- residence-sw.py
+++ residence-sw.py
@@ -5,55 +5,49 @@ import telnetlib
 import time
 
 username = "cisco"
-unamepw = "telnetpassword"
+unamepw = "cisco"
 ip = "192.168.1.254"
 
 tn = telnetlib.Telnet(ip)
 time.sleep(2)
-response = tn.read_until(b"User Name:", 5)
+response = tn.read_until(b":", 5)
 print(response)
 
-tn.write(username.encode('ascii') + b"\n")
-output = tn.read_until(b":", 5)
-print(output)
-tn.write(unamepw.encode('ascii') + b"\n")
-output = tn.read_until(b"#", 5)
-print(output)
+if b"Username" in response:
+	print("found")
+	tn.write(username.encode('ascii') + b"\n")
+	output = tn.read_until(b":", 5)
+	print(output)
+	tn.write(unamepw.encode('ascii') + b"\n")
+	output = tn.read_until(b">", 5)
+	print(output)
+	tn.write(b"enable" + b"\n")
+	output = tn.read_until(b":", 5)
+	print(output)
+	tn.write(unamepw.encode('ascii') + b"\n")
+	output = tn.read_until(b"#", 5)
+	print(output)
+	
+else:
+	print("not found")
 
+#tn.write(b"show vlan" + b"\n")
+#output = tn.read_until(b"#", 5)
+#print(output)
 
 tn.write(b"configure terminal" + b"\n")
 output = tn.read_until(b"#", 5)
 print(output)
 
-tn.write(b"hostname residence-sw" + b"\n")
+tn.write(b"vlan 222" + b"\n")
 output = tn.read_until(b"#", 5)
 print(output)
 
-tn.write(b"vlan 20" + b"\n")
+tn.write(b"end" + b"\n")
 output = tn.read_until(b"#", 5)
 print(output)
 
-tn.write(b"exit" + b"\n")
-output = tn.read_until(b"#", 5)
-print(output)
-
-tn.write(b"spanning-tree vlan 20 root primary" + b"\n")
-output = tn.read_until(b"#", 5)
-print(output)
-
-tn.write(b"int e1/0" + b"\n")
-output = tn.read_until(b"#", 5)
-print(output)
-
-tn.write(b"switchport mode access" + b"\n")
-output = tn.read_until(b"#", 5)
-print(output)
-
-tn.write(b"switchport access vlan 20" + b"\n")
-output = tn.read_until(b"#", 5)
-print(output)
-
-tn.write(b"sh run" + b"\n")
+tn.write(b"show vlan" + b"\n")
 output = tn.read_until(b"#", 5)
 print(output)
 
