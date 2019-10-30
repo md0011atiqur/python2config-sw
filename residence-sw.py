f74bb151a15fb413e334b0714abc90c1e2be504b
Md Atiqur Rahman <mdatiqurrahman90@gmail.com>
2019-10-30 13:28:03 +0600

    2nd commint

--- residence-sw.py
+++ residence-sw.py
@@ -13,19 +13,13 @@ time.sleep(2)
 response = tn.read_until(b"User Name:", 5)
 print(response)
 
-if b"User Name:" in response:
-	print("found")
-	tn.write(username.encode('ascii') + b"\n")
-	output = tn.read_until(b":", 5)
-	print(output)
-	tn.write(unamepw.encode('ascii') + b"\n")
-	output = tn.read_until(b"#", 5)
-	print(output)
-	
-else:
-	print("not found")
-	tn.write(telnetpw.encode('ascii') + b"\n")
-	tn.read_until(b">", 5)
+tn.write(username.encode('ascii') + b"\n")
+output = tn.read_until(b":", 5)
+print(output)
+tn.write(unamepw.encode('ascii') + b"\n")
+output = tn.read_until(b"#", 5)
+print(output)
+
 
 tn.write(b"configure terminal" + b"\n")
 output = tn.read_until(b"#", 5)
